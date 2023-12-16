d871bcd5ae12baa407b5e5789396a36ce4bad7dd
Akash Singh <128240203+Ak21218@users.noreply.github.com>
2023-12-16 08:52:28 +0530

    Add files via upload

---
+++ string1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class string1 {
+    public static void main(String args[]) {
+         intro_str();
+    
+    }
+
+    public static void intro_str() {
+        Scanner sc = new Scanner(System.in);
+        String name = "Tony Stark";
+        System.out.println("Name:- " + name);
+    }
+}
