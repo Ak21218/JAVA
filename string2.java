c450c59e40a4d1ce38a29cf07ad228b85ac93a0e
Akash Singh <128240203+Ak21218@users.noreply.github.com>
2023-12-17 08:46:16 +0530

    Add files via upload

---
+++ string2.java
@@ -0,0 +1,14 @@
+// next() takes till the 1st string
+        // nextLine() takes till enter is pressed
+
+import java.util.*;
+
+public class string2 {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the name:- ");
+        String string2 = sc.nextLine();
+        System.out.println("The name is:- " + string2);
+    }
+}
