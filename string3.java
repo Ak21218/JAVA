c724b25df0245566c3365c047915d569a098b88a
Akash Singh <128240203+Ak21218@users.noreply.github.com>
2023-12-18 08:19:24 +0530

    Add files via upload

---
+++ string3.java
@@ -0,0 +1,12 @@
+public class string3 {
+    public static void main(String args[]) {
+
+           String a = "tony stark";
+           String b = "tony";
+
+    if (a.equals(b) == true)
+            System.out.println("Both the names are same");
+        else
+            System.out.println("Not same");
+}
+}
