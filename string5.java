5e8f4b4d14e572e4850d4558ba2c711309a532aa
Akash Singh <128240203+Ak21218@users.noreply.github.com>
2023-12-19 12:08:09 +0530

    Add files via upload

---
+++ string5.java
@@ -0,0 +1,14 @@
+public class string5 {
+    
+    public class string4 {
+        public static void main(String[] args) {
+    
+    
+            String a = "AKASH Singh";
+            String b = "Tony";
+    
+    System.out.println("Length of " + a + " is:- " + a.length());
+        System.out.println("Length of " + a + " is:- " + b.length());
+        }
+    }
+}
