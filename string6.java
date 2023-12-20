2d3e4b93aeb74dd14a74612d5132d8fef4d5a959
Akash Singh <128240203+Ak21218@users.noreply.github.com>
2023-12-20 19:44:16 +0530

    Add files via upload

---
+++ string6.java
@@ -0,0 +1,13 @@
+//to merg the 2 char.
+public class string6 {
+        public static void main(String[] args) {
+    
+    
+            String a = "Tony";
+            String b = "Stark";
+
+            a=a+" "+b;
+            System.out.println(a);
+        }
+    }
+
