f061b6bfd5ddf22b4e1a07c79e2a9fc10bfb94db
Akash Singh <128240203+Ak21218@users.noreply.github.com>
2023-12-21 20:34:37 +0530

    Add files via upload

---
+++ string7.java
@@ -0,0 +1,13 @@
+//To find the name is a subset of name2 or not
+public class string7 {
+    public static void main(String[] args) {
+
+        String a = "Tony";
+        String b = "Tony Singh";
+
+        if (b.contains(a))
+            System.out.println("It contains");
+        else
+            System.out.println("Not contain");
+    }
+}
