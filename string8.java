eafbc143dc5e079e36f054659d6f8166ce9c180c
Akash Singh <128240203+Ak21218@users.noreply.github.com>
2023-12-22 09:37:25 +0530

    Add files via upload

---
+++ string8.java
@@ -0,0 +1,11 @@
+public class string8 {
+    public static void main(String[] args) {
+
+        String a = "Tony";
+
+
+        System.out.println(a.charAt(1));
+        System.out.println(a.indexOf("n"));
+        System.out.println(a.replace("n", "m"));
+    }
+}
