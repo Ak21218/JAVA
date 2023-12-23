5acaede1dabe101f1ee13adf203b3b141a87206c
Akash Singh <128240203+Ak21218@users.noreply.github.com>
2023-12-23 20:25:06 +0530

    Add files via upload

---
+++ string9.java
@@ -0,0 +1,12 @@
+public class string9 {
+    public static void main(String[] args) {
+        String b = "Tony Singh";
+
+        System.out.println(b.substring(2));
+        System.out.println(b.substring(2, 5));
+        System.out.println(b.startsWith("St"));
+        System.out.println(b.endsWith("gh"));
+        System.out.println(b.toUpperCase());
+        System.out.println(b.toLowerCase());
+}
+}
