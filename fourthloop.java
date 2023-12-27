a1ac83860a09a72fdf0fd9d7c5220344e490340d
Akash Singh <128240203+Ak21218@users.noreply.github.com>
2023-12-27 08:18:41 +0530

    Add files via upload

---
+++ fourthloop.java
@@ -0,0 +1,15 @@
+public class fourthloop {
+    public static void main(String[] args){
+
+    
+    // do while loops
+    int i = 12;
+do{System.out.println(i);
+    i=i+1;
+}while(i<11);
+
+do{
+        System.out.println("apna collage");
+    }while(i<11);
+}
+}
