11efd0c164ff610426a7334a98752cf84230d102
Akash Singh <128240203+Ak21218@users.noreply.github.com>
2023-12-28 08:43:44 +0530

    Add files via upload

---
+++ secoundloop.java
@@ -0,0 +1,10 @@
+public class secoundloop {
+    public static void main(String args[]) {
+        // print the number from 0 to 10
+        
+        for(int i = 0; i < 10 ; i++) {
+            System.out.println(i+" ");
+             }
+             }
+             }
+
